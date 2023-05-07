221f69a8d241e2619f7b9b44e4cf20c227dd71f4
PilarVF <115066310+PilarVF@users.noreply.github.com>
2023-05-08 00:14:28 +0200

    Index, saludos y ta-te-ti

---
+++ saludos.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Variable que usamos para pintar los saludos:
+var colorfuente = "white"
+
+// Array con colores, tipo constante, para definir los colores de los cuadritos: 
+const colores = [];
+colores[0] = "rgb(250, 78, 78)";
+colores[1] = "rgb(68, 160, 252)";
+colores[2] = "rgb(236, 236, 198)";
+colores[3] = "rgb(32, 206, 32)";
+colores[4] = "rgb(254, 254, 40)";
+
+// Función que sirve para mostrar un mensaje en un elemento:
+function saludar(identificador) {
+    let elem = document.getElementById(identificador);
+    elem.style.color = colorfuente;
+    elem.innerHTML = "Hello there!"
+    colorfuente = getComputedStyle(elem).backgroundColor;
+}
+
+// Función que sirve para elegir elementos a pintar y colores:
+function pintar() {
+    for (let x = 1; x <= colores.length; x++) {
+        let ident = "c" + x;
+        let cf = colores[x - 1];
+        rellenar(ident, cf);
+    }
+}
+
+// Función que sirve para pintar un cuadrito:
+function rellenar(identificador, colorfondo) {
+    let elem = document.getElementById(identificador);
+    elem.style.backgroundColor = colorfondo;
+}
+
+pintar();
---
+++ ta-te-ti.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// Función que sirve para que una casilla cambie a color gris claro al poner sobre ella el cursor del ratón:
+function ponerRaton(id) {
+    document.getElementById(id).style.backgroundColor = "rgb(244, 246, 246)";
+}
+
+// Función que sirve para que una casilla vuelva a su color original (blanco) al apartar de ella el cursor del ratón:
+function quitarRaton(id) {
+    document.getElementById(id).style.backgroundColor = "white";
+}
+
+let turno = 0; // Turno de la partida
+
+const gato = '😸'; // Emoticono de gato para Jugador 1
+const perro = '🐶'; // Emoticono de perro para Jugador 2
+
+// Combinaciones ganadoras:
+var paraGanar = [[0, 1, 2], [3, 4, 5], [6, 7, 8], // En horizontal
+                 [0, 3, 6], [1, 4, 7], [2, 5, 8], // En vertical
+                 [0, 4, 8], [2, 4, 6] // En diagonal
+                ];
+
+const posicion_btn = document.querySelectorAll(".cuadrito"); // Posición del tablero
+
+// Función que sirve para colocar un emoticono en una casilla al hacer click en ella:
+function clicar(id) {
+    turno++;
+    const emoticono = turno % 2 ? gato : perro; // Cuando el turno es impar, coloca el emoticono de gato. Cuando es par, el de perro.
+    let elemento = document.getElementById(id);
+    elemento.innerHTML = emoticono;
+    verificar();
+}
+
+// Función que sirve para recorrer el array de combinaciones ganadoras y verificar si esas posiciones del tablero contienen el mismo emoticono:
+function verificar() {
+    for (var i = 0; i < paraGanar.length; i++) {
+        if (posicion_btn[paraGanar[i][0]].innerHTML == gato && posicion_btn[paraGanar[i][1]].innerHTML == gato && 
+            posicion_btn[paraGanar[i][2]].innerHTML == gato) {
+            alert('Ganador: 😸');
+            reiniciar();
+            return;
+        } else if (posicion_btn[paraGanar[i][0]].innerHTML == perro && posicion_btn[paraGanar[i][1]].innerHTML == perro && 
+                   posicion_btn[paraGanar[i][2]].innerHTML == perro) {
+            alert('Ganador: 🐶');
+            reiniciar();
+            return;
+        }
+    }
+
+    // Para verificar que ninguna casilla del juego está vacía. Si no gana ningún jugador, se produce un empate:
+    if (posicion_btn[0].innerHTML != "" && posicion_btn[1].innerHTML != "" && posicion_btn[2].innerHTML != "" && 
+        posicion_btn[3].innerHTML != "" && posicion_btn[4].innerHTML != "" && posicion_btn[5].innerHTML != "" && 
+        posicion_btn[6].innerHTML != "" && posicion_btn[7].innerHTML != "" && posicion_btn[8].innerHTML != "") {
+        alert('Empate.');
+        reiniciar();
+    }
+}
+
+// Función que sirve para que todas las casillas vuelvan a estar vacías:
+function reiniciar() {
+    turno = 0;
+    posicion_btn.forEach(boton => {
+        boton.innerHTML = '';
+    });
+}
